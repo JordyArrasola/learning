360b5ae5bfef4f3fb3bcdc37cd4e64b9218f4e9a
Jordy Arrasola <65517723+JordyArrasola@users.noreply.github.com>
2023-01-20 23:03:30 +0000

    Session one material

---
+++ Practice.py
@@ -0,0 +1,20 @@
+first = "Jordy" 
+last = "Arrasola"
+full_name = first + " " + last
+print(full_name)
+
+
+one = "sugondese"
+two = "Loob"
+
+print(one + " " + two)
+
+
+test = 6.9 
+
+print(int(test))
+
+
+test = 69
+
+print(float(test))
